6536b7a045e227b132c1863500824fd5feebcc5d
Bartosz Bednarski <bambocha1997@gmail.com>
2022-05-28 11:08:45 +0200

    Updated data base of movies

--- script.js
+++ script.js
@@ -32,6 +32,12 @@ const filmy = {
 };
 
 const fruits = ['jabko', 'adam', 'ewa'];
-document.getElementById("skazaniNaShawshank").innerHTML = `<img src="film1.png"> Skazani na Shawshank <br><br>Reżyser: ${filmy.skazaniNaShawshank.rezyser} <br> Premiera: ${filmy.skazaniNaShawshank.premiera} <br> Fabuła: ${filmy.skazaniNaShawshank.fabula}`;
+document.getElementById("zielonaMila").innerHTML = `<img src="/okladki/zM.png"> Zielona Mila <br><br>Reżyser: ${filmy.zielonaMila.rezyser} <br> Premiera: ${filmy.zielonaMila.premiera} <br> Fabuła: ${filmy.zielonaMila.fabula}`;
+document.getElementById("skazaniNaShawshank").innerHTML = `<img src="/okladki/sNs.jpg"> Skazani na Shawshank <br><br>Reżyser: ${filmy.skazaniNaShawshank.rezyser} <br> Premiera: ${filmy.skazaniNaShawshank.premiera} <br> Fabuła: ${filmy.skazaniNaShawshank.fabula}`;
+document.getElementById("forrestGump").innerHTML = `<img src="/okladki/fG.jpg"> Forrest Gump <br><br>Reżyser: ${filmy.forrestGump.rezyser} <br> Premiera: ${filmy.forrestGump.premiera} <br> Fabuła: ${filmy.forrestGump.fabula}`;
+document.getElementById("gladiator").innerHTML = `<img src="/okladki/glad.jpg"> Fladiator <br><br>Reżyser: ${filmy.gladiator.rezyser} <br> Premiera: ${filmy.gladiator.premiera} <br> Fabuła: ${filmy.gladiator.fabula}`;
+document.getElementById("szeregowiecRyan").innerHTML = `<img src="/okladki/sR.jpg"> Szeregowiec Ryann <br><br>Reżyser: ${filmy.szeregowiecRyan.rezyser} <br> Premiera: ${filmy.szeregowiecRyan.premiera} <br> Fabuła: ${filmy.szeregowiecRyan.fabula}`;
+document.getElementById("pulpFiction").innerHTML = `<img src="/okladki/pF.jpg"> Pulp Fiction <br><br>Reżyser: ${filmy.pulpFiction.rezyser} <br> Premiera: ${filmy.pulpFiction.premiera} <br> Fabuła: ${filmy.pulpFiction.fabula}`;
+
 console.log(filmy.pulpFiction);
 
